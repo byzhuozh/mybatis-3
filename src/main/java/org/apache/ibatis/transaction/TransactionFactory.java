842c43dca7d7f91dd4e733843f81f89e53bd85ac
weiqi <Abc123456>
2019-04-02 11:27:40 +0800

    事务模块

--- src/main/java/org/apache/ibatis/transaction/TransactionFactory.java
+++ src/main/java/org/apache/ibatis/transaction/TransactionFactory.java
@@ -26,17 +26,24 @@ import org.apache.ibatis.session.TransactionIsolationLevel;
  * Creates {@link Transaction} instances.
  *
  * @author Clinton Begin
+ *
+ * Transaction 工厂接口
  */
 public interface TransactionFactory {
 
   /**
    * Sets transaction factory custom properties.
-   * @param props
+   * 设置工厂的属性
+   *
+   * @param props 属性
    */
   void setProperties(Properties props);
 
   /**
    * Creates a {@link Transaction} out of an existing connection.
+   *
+   * 创建 Transaction 事务
+   *
    * @param conn Existing database connection
    * @return Transaction
    * @since 3.1.0
@@ -45,6 +52,9 @@ public interface TransactionFactory {
   
   /**
    * Creates a {@link Transaction} out of a datasource.
+   *
+   * 创建 Transaction 事务
+   *
    * @param dataSource DataSource to take the connection from
    * @param level Desired isolation level
    * @param autoCommit Desired autocommit
