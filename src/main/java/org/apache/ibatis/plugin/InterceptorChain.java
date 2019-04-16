355ff34dc554d5ab52a9504d2c231d5737e1fced
weiqi <Abc123456>
2019-04-16 16:32:54 +0800

    插件体系

--- src/main/java/org/apache/ibatis/plugin/InterceptorChain.java
+++ src/main/java/org/apache/ibatis/plugin/InterceptorChain.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2018 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2018 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.plugin;
 
@@ -21,24 +21,36 @@ import java.util.List;
 
 /**
  * @author Clinton Begin
+ * <p>
+ * 拦截器 Interceptor 链
  */
 public class InterceptorChain {
 
-  private final List<Interceptor> interceptors = new ArrayList<>();
+    /**
+     * 拦截器数组
+     */
+    private final List<Interceptor> interceptors = new ArrayList<>();
 
-  public Object pluginAll(Object target) {
-    for (Interceptor interceptor : interceptors) {
-      target = interceptor.plugin(target);
+    /**
+     * 应用所有拦截器到指定目标对象：
+     * 被 Configuration 的如下四处方法中调用：
+     *      一共可以有四种目标对象类型可以被拦截：1）Executor；2）StatementHandler；3）ParameterHandler；4）ResultSetHandler
+     * eg: http://static2.iocoder.cn/images/MyBatis/2020_03_18/03.png
+     */
+    public Object pluginAll(Object target) {
+        for (Interceptor interceptor : interceptors) {
+            target = interceptor.plugin(target);
+        }
+        return target;
     }
-    return target;
-  }
 
-  public void addInterceptor(Interceptor interceptor) {
-    interceptors.add(interceptor);
-  }
-  
-  public List<Interceptor> getInterceptors() {
-    return Collections.unmodifiableList(interceptors);
-  }
+    // 添加拦截器
+    public void addInterceptor(Interceptor interceptor) {
+        interceptors.add(interceptor);
+    }
+
+    public List<Interceptor> getInterceptors() {
+        return Collections.unmodifiableList(interceptors);
+    }
 
 }
